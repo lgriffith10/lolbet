c70b1ab5708ebc7c31245287bb0dc37bbb41c3a0
lgriffith10 <lgriffith.dev@gmail.com>
2025-10-22 21:03:19 +0200

    feat(user): add user auth and controller handling

---
+++ apps/backend/tests/validators/auth.spec.ts
@@ -0,0 +1,16 @@
+﻿import { test } from '@japa/runner'
+import { loginValidator } from '#validators/auth'
+
+test.group('Auth - loginValidator', () => {
+  test('valide correctement un payload valide', async ({ assert }) => {
+    const payload = {
+      fullName: 'John Doe',
+      email: 'john@example.com',
+      password: 'securepassword123',
+    }
+
+    const result = await loginValidator.validate(payload)
+    assert.equal(result.email, 'john@example.com')
+    assert.equal(result.password, 'securepassword123')
+  })
+})
